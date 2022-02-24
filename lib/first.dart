2797a548ec723970496f4a462765587223afaa03
pimphand <faisaldwiki69@gmail.com>
2022-02-24 16:41:35 +0700

    sek akeh seng kurang

--- lib/first.dart
+++ lib/first.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class First extends StatelessWidget {
   const First({Key? key}) : super(key: key);
@@ -94,106 +95,77 @@ class First extends StatelessWidget {
     Widget bottom() {
       return Container(
         height: size.height * 0.4,
+        width: size.width,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(size.height * 0.05),
-            topRight: Radius.circular(size.height * 0.05),
-          ),
-          color: Colors.white24,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 10,
-              spreadRadius: 5,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(size.height * 0.05),
+              topRight: Radius.circular(size.height * 0.05),
             ),
-          ],
-        ),
-        child: SingleChildScrollView(
+            color: Colors.white),
+        child: PageView.builder(
           scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                height: size.height * 0.2,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(size.height * 0.05),
-                    topRight: Radius.circular(size.height * 0.05),
-                  ),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      blurRadius: 10,
-                      spreadRadius: 5,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Welcome to',
-                      style: TextStyle(
-                        fontSize: size.height * 0.03,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: size.height * 0.01),
-                    Text(
-                      'The App',
-                      style: TextStyle(
-                        fontSize: size.height * 0.03,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
+          itemCount: 3,
+          itemBuilder: (_, index) {
+            return Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: size.height * 0.04,
+                vertical: size.height * 0.04,
               ),
-              SizedBox(height: size.height * 0.02),
-              Container(
-                height: size.height * 0.2,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(size.height * 0.05),
-                    topRight: Radius.circular(size.height * 0.05),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Read the article you want instantly",
+                          style: GoogleFonts.montserrat(
+                            fontSize: size.height * 0.03,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(height: size.height * 0.01),
+                        Text(
+                          "You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones.",
+                          style: GoogleFonts.montserrat(
+                            fontSize: size.height * 0.02,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.03,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: size.width * 0.3,
+                              height: size.height * 0.09,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color(0xff376AED),
+                              ),
+                              child: Image.asset(
+                                'assets/arrow--right.png',
+                                height: 20,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      blurRadius: 10,
-                      spreadRadius: 5,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'The App',
-                      style: TextStyle(
-                        fontSize: size.height * 0.03,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: size.height * 0.01),
-                    Text(
-                      'is a place where you can find',
-                      style: TextStyle(
-                        fontSize: size.height * 0.03,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       );
     }
 
     return Scaffold(
+      backgroundColor: Color(0xffE9EDF9),
       body: Column(
         children: [
           header(),
